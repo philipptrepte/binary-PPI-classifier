4ba0cac57c00dd246755c482dde17c35391d5d33
philipptrepte <93270384+philipptrepte@users.noreply.github.com>
2023-09-26 11:30:55 +0200

    debugging

--- R/plot_probDis.R
+++ R/plot_probDis.R
@@ -14,13 +14,13 @@ probDis.plot <- function(ppi_prediction_result, set="test") {
   assertthat::assert_that(set %in% c("test", "train"), msg = "'set=' must be 'test' or 'train'.")
   if(set == "test") {
     plot <- ppi_prediction_result$predDf %>%
-      ggplot2::ggplot(aes(x = mean_cBRET, y = predMat, fill = complex)) +
+      ggplot2::ggplot(aes(x = !!rlang::sym(ppi_prediction_result$assay[1]), y = predMat, fill = complex)) +
       ggplot2::geom_point(shape = 21, alpha = 0.7, size = 1.5) +
       ggpubr::theme_pubr() +
       ggplot2::theme(legend.position = "none") +
       viridis::scale_fill_viridis(discrete = TRUE, option = "D") +
       ggplot2::scale_y_continuous(breaks = c(0, 0.25, 0.5, 0.75, 1), limits = c(0,1)) +
-      ggplot2::labs(x = "cBRET ratio", y = "probability") +
+      ggplot2::labs(x = ppi_prediction_result$assay[1], y = "probability") +
       ggplot2::theme(text = element_text(family = "Avenir"),
                      plot.title = element_text(size = 12),
                      plot.subtitle = element_text(size = 8),
@@ -28,13 +28,13 @@ probDis.plot <- function(ppi_prediction_result, set="test") {
                      legend.position = "right")
   } else if(set == "train") {
     plot <- ppi_prediction_result$predTrainDf %>%
-      ggplot2::ggplot(aes(x = mean_cBRET, y = predTrainMat, fill = reference)) +
+      ggplot2::ggplot(aes(x = !!rlang::sym(ppi_prediction_result$assay[1]), y = predTrainMat, fill = reference)) +
       ggplot2::geom_point(shape = 21, alpha = 0.7, size = 1.5) +
       ggpubr::theme_pubr() +
       ggplot2::theme(legend.position = "none") +
       ggplot2::scale_fill_manual(values = c("#6CA6C1", "#D930C5")) +
       ggplot2::scale_y_continuous(breaks = c(0, 0.25, 0.5, 0.75, 1), limits = c(0,1)) +
-      ggplot2::labs(x = "cBRET ratio", y = "probability") +
+      ggplot2::labs(x = ppi_prediction_result$assay[1], y = "probability") +
       ggplot2::theme(text = element_text(family = "Avenir"),
                      plot.title = element_text(size = 12),
                      plot.subtitle = element_text(size = 8),
